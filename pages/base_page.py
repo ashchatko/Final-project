c496edb03aee728101c4bedf1540b5915b2e1a30
Anatoli Shchatko <Anatoli_Shchatko@STYLESOFT.BY>
2022-04-09 13:10:21 +0300

    base_page and main_page were updated

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,15 @@ class BasePage():
   def __init__(self, browser, url):
     self.browser = browser
     self.url = url
+    self.browser.implicitly_wait(timeout)
+    from selenium.common.exceptions import NoSuchElementException
   
   def open(self):
     self.browser.get(self.url)
+    
+  def is_element_present(self, how, what):
+    try:
+         self.browser.find_element(how, what)
+    except (NoSuchElementException):
+         return False
+    return True
